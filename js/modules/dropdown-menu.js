f11b7f0929aa3eb067e0fa28feee50617afeaae8
Marlyson dos Santos Mendes <50031055+MarlysonMendes@users.noreply.github.com>
2021-08-25 00:41:01 -0300

    Mobile Menu

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,7 +1,9 @@
 import outsideClick from './outsideclick.js';
 
 export default function initDropdownMenu() {
+  //procuro por todos os itens que possuem dropdown
   const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+  //adiciono os eventos touchstart e click nos dropdownmenu
   dropdownMenus.forEach(menu => {
     ['touchstart', 'click'].forEach(userEvent => {
       menu.addEventListener(userEvent, handleClick);
@@ -9,8 +11,11 @@ export default function initDropdownMenu() {
   });
 
   function handleClick(event) {
+    //evito o funcionamento padrão abrir a pagina quando clicko
     event.preventDefault();
+    //o this é quem disparou o evento eu adiciono a classe active para aparecer o menu
     this.classList.add('active');
+    //funcao que remove o menu quando clicko do lado de de fora
     outsideClick(this, ['touchstart', 'click'], () => {
       this.classList.remove('active');
     });
