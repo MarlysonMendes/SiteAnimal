453c94055f6870c5bf52a917468b1cd2e9ebe24a
Marlyson dos Santos Mendes <50031055+MarlysonMendes@users.noreply.github.com>
2021-08-25 01:16:03 -0300

    AnimaNumero
    MutationObserver

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,9 +1,7 @@
 import outsideClick from './outsideclick.js';
 
 export default function initDropdownMenu() {
-  //procuro por todos os itens que possuem dropdown
   const dropdownMenus = document.querySelectorAll('[data-dropdown]');
-  //adiciono os eventos touchstart e click nos dropdownmenu
   dropdownMenus.forEach(menu => {
     ['touchstart', 'click'].forEach(userEvent => {
       menu.addEventListener(userEvent, handleClick);
@@ -11,11 +9,8 @@ export default function initDropdownMenu() {
   });
 
   function handleClick(event) {
-    //evito o funcionamento padrão abrir a pagina quando clicko
     event.preventDefault();
-    //o this é quem disparou o evento eu adiciono a classe active para aparecer o menu
     this.classList.add('active');
-    //funcao que remove o menu quando clicko do lado de de fora
     outsideClick(this, ['touchstart', 'click'], () => {
       this.classList.remove('active');
     });
